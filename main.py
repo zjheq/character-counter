b0bf8cb9213471c21671f97b8563fc407dffce26
zj <zanjoesoliveres26@gmail.com>
2026-04-01 22:31:48 +0800

    asd

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import pyclip
 
-
-message = 'I\'m heavy, I\'m by your side\nforget me \' cause I know what I need\nlike a loser like me will be fine'
-
+message = pyclip.paste()
 count = {}
+total = 0
 
 for i in message:
     count.setdefault(i, 0)
     count[i] = count[i] + 1
     
+for i in message:
+    total +=1
+    print(f"{i} {total}")
+    
 print(count)
+print(total)
